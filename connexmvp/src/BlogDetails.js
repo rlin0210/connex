49522ad20146c2ad3f3f3aafdfed2e1ef168022a
Richard Lin <rlin24@berkeley.edu>
2022-11-15 19:43:13 -0800

    restarting the format

--- connexmvp/src/BlogDetails.js
+++ connexmvp/src/BlogDetails.js
@@ -1,43 +1,29 @@
 import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import React from "react";
 
 const BlogDetails = () => {
   const { id } = useParams();
   const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
   const history = useHistory();
 
-
   return (
-    <div className='IndividualView'>
-			<div className='OverallBackground'>
-			</div>
-			<div className='Essay2Group'>
-				<div className='Essay2Box'/>
-				<span className='Essay2Texts'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br/><br/>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
-				<span className='Essay2Title'>Essay 2</span>
-			</div>
-			<div className='Essay1Group'>
-				<div className='Essay1Box'/>
-				<span className='Essay1Text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br/><br/>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
-				<span className='Essay1Title'>Essay 1</span>
-			</div>
-			<div className='AttachmentsGroup'>
-				<div className='AttachmentsBox'/>
-				<span className='AttachmentsText'>LinkedIn: linkedin.com/sfasdf<br/>Other: mypersonalwebsite.com<br/>Video: link</span>
-				<span className='AttachmentsTitle'>Attachments:</span>
-			</div>
-			<div className='ReferralsGroup'>
-				<div className='ReferralsBox'/>
-				<span className='ReferralsText'>Chris Bernard<br/>Thought they were very engaging<br/><br/>Richard Lin<br/>They were incredibly social and genuinely interested in the club<br/></span>
-				<span className='ReferralsTitle'>Referrals:</span>
-			</div>
-			<div className='ProfileGroup'>
-				{/* <img className='ProfilePicture' src = {ImgAsset.ApplicationsClubApplicants_image6} /> */}
-				<span className='ContactInfo'>James Chong<br/>jameschong@berkeley.edu<br/>+1 (510) 999 4356</span>
-			</div>
-			{/* <img className='MiddleLine' src = {ImgAsset.IndividualView_1_MiddleLine} />
-			<img className='Resume' src = {ImgAsset.IndividualView_JamesChongResumeFall20221} /> */}
-		</div>
+    <div class="overall">
+      <div class="left">
+          left hand side
+      </div>
+      <div class="right">
+        <div class="top">
+          should be top
+        </div>
+        <div class="middle">
+          should be middle
+        </div>
+        <div class="bottom">
+          should be bottom
+        </div>
+      </div>
+    </div>
   );
 }
  
