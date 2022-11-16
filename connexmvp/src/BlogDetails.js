9491bda25f1b99072cadf87cbdef2040f0c94723
Richard Lin <rlin24@berkeley.edu>
2022-11-15 20:15:12 -0800

    finished template

--- connexmvp/src/BlogDetails.js
+++ connexmvp/src/BlogDetails.js
@@ -14,7 +14,43 @@ const BlogDetails = () => {
       </div>
       <div class="right">
         <div class="top">
-          should be top
+          <div class="ProfileGroup">
+            profile
+            <div class="ProfilePic">
+
+            </div>
+            <div class="ContactInfo">
+
+            </div>
+          </div>
+          <div class="OtherGroup">
+            <div class="ReferralGroup">
+              <div class="ReferralTitle">
+                Referrals:
+              </div>
+              <div class="ReferralBody">
+                Chris Bernard<br></br>
+                Thought they were very engaging
+                <br></br>
+                <br></br>
+                Richard Lin<br></br>
+                They were incredibly social and genuinely interested in the club
+              </div>
+            </div>
+            <div class="AttachmentGroup">
+            <div class="ReferralTitle">
+                Attachments:
+              </div>
+              <div class="ReferralBody">
+                LinkedIn: linkedin.com/sfasdf
+                <br></br>
+                Other: mypersonalwebsite.com
+                <br></br>
+                Video: link
+              </div>
+            </div>
+
+          </div>
         </div>
         <div class="middle">
           should be middle
