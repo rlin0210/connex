121149c1c27d9ef1ee6300b32704d7ee89c41594
Christian T. Bernard <chrisbert@berkeley.edu>
2022-11-25 15:48:25 -0800

    Might have the css done fr frfr

--- connexmvp/src/BlogDetails.js
+++ connexmvp/src/BlogDetails.js
@@ -5,11 +5,12 @@ import Pdf from "./PDF";
 
 const BlogDetails = () => {
   const { id } = useParams();
-  const { data: application, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+  const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
   const history = useHistory();
 
   return (
     <div class="overall">
+<<<<<<< HEAD
       { isPending && <div>Loading...</div> }
       { error && <div>{ error }</div> }
       { application && (
@@ -68,13 +69,59 @@ const BlogDetails = () => {
             <div class="bottom">
               { application.essay2 }
             </div>
+=======
+      <div class="left">
+          left hand side
+      </div>
+      <div class="right">
+        <div class="top">
+          <div class="ProfileGroup">
+            <div class="ProfilePic">              
+            <img
+                style={{ maxWidth: 250 }}
+                src="https://media.istockphoto.com/id/1262964438/photo/success-happens-the-moment-you-believe-it-will.jpg?s=612x612&w=0&k=20&c=tpjbR4aaaiB43sneEWgatyFIQOmN3E-3nB5CBE5Idyg="
+                alt="Headshot"
+              />
+>>>>>>> c9234f1 (Might have the css done fr)
             </div>
+            <div class="ContactInfo">
+              contact information
             </div>
           </div>
-        </article>
-      )}
-
-    
+          <div class="OtherGroup">
+            <div class="ReferralGroup">
+              <div class="ReferralTitle">
+                Referrals:
+              </div>
+              <div class="ReferralBody">
+                referral text
+              </div>
+            </div>
+            <div class="AttachmentGroup">
+              <div class="AttachmentTitle">
+                  Attachments:
+              </div>
+              <div class="AttachmentBody">
+                attachment text
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="EssayGroup">
+        <div class="Essay1Group">
+                Essay 1 Prompt:
+        <div class="middle">
+          Essay 1 Response Body
+        </div>
+        </div>
+        <div class="Essay2Group">
+                Essay 2 Prompt:
+        <div class="bottom">
+        Essay 2 Response Body
+        </div>
+        </div>
+        </div>
+      </div>
     </div>
   );
 }
