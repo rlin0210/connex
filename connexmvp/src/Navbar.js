4a732b96e858ebc7e4d4dbc585517ad3feaa488e
Richard Lin <rlin24@berkeley.edu>
2022-12-31 00:22:36 -0600

    finished navbar

--- connexmvp/src/Navbar.js
+++ connexmvp/src/Navbar.js
@@ -1,17 +1,32 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import connex from "./icons/logo.png";
+import setting from "./icons/setting.png"
+import home from "./icons/home.png"
+import compare from "./icons/compare.png"
+
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Use. Connex.</h1>
+      <img src={connex} alt="connex logo" className="logo"/>
+
       <div className="links">
-        <Link to="/">Home</Link>
-        <Link to="/create" style={{
-          color: 'white', 
-          backgroundColor: '#3a73f8',
-          borderRadius: '8px' 
-        }}>Create Application</Link>
+        <div className="container">
+          <img src={home} alt="home icon" className="icons"/>
+          <Link to="/">Home</Link>
+        </div>
+        <div className="container">
+          <img src={compare} alt="home icon" className="icons"/>
+          <Link to="/">Compare</Link>
+        </div>
+        <div className="container">
+          <img src={setting} alt="home icon" className="icons"/>
+          <Link to="/">Settings</Link>
+        </div>
+        {/* <div className="container">
+          <Link to="/create">Create</Link>
+        </div> */}
       </div>
     </nav>
   );
