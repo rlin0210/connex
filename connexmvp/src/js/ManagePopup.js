40a955fec88b001c885e20296807f5b3aa28acf5
Richard Lin <rlin24@berkeley.edu>
2023-01-01 21:01:31 -0600

    added primitive toggle

--- connexmvp/src/js/ManagePopup.js
+++ connexmvp/src/js/ManagePopup.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../css/ManagePopup.css";
+import Toggle from "./Toggle";
 
 const ManagePopup = (props) => {
     return (
@@ -7,6 +8,7 @@ const ManagePopup = (props) => {
             <div className="box">
                 <span className="close-icon" onClick={props.handleClose}>x</span>
                 Contains the Manage Column Elements
+                <Toggle label="Scoring"/>
             </div>
         </div>
     );
