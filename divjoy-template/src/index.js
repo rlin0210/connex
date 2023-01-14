bb927d176a6ba4026cfe30316b98494829a29bbb
Richard Lin <rlin24@berkeley.edu>
2023-01-13 21:19:13 -0800

    redownloaded file

--- divjoy-template/src/index.js
+++ divjoy-template/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-// import App from "./pages/_app";
-import App from "./original/js/App"
+import App from "./pages/_app";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDom.render(<App />, document.getElementById("root"));
